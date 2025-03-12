36b034781de6bdaf603669de6d66e1c1275059fd
THANGAJEBINSONA <thangajebinson2006@gmail.com>
2025-03-12 15:25:35 +0530

    concat1

---
+++ concat2.java
@@ -0,0 +1,9 @@
+public class concat2
+{
+    public static void main(String []args)
+    {
+        String str3="Gold";
+        String str4="Jebin";
+        System.out.println(str3.concat(str4));
+    }
+}

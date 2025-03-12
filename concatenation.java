590db0676f258e99b827587a3dda4c049b52706d
THANGAJEBINSONA <thangajebinson2006@gmail.com>
2025-03-12 15:21:37 +0530

    concatenation+

---
+++ concatenation.java
@@ -0,0 +1,10 @@
+public class concatenation
+{
+    public static void main(String []args)
+    {
+        String str1="Thanga";
+        String str2="Jebinson";
+        System.out.println(str1+str2);
+       
+    }
+}

e120ba2f8ddc9fdb1019090d0fbb91ee99363d19
THANGAJEBINSONA <thangajebinson2006@gmail.com>
2025-03-13 11:09:47 +0530

    arithmetic function

---
+++ arithmetic.java
@@ -0,0 +1,10 @@
+public class arithmetic{
+    public static void main(String []args)
+    {
+        int a=10;
+        int b=5;
+        System.out.println("Add:" +(a+b));
+        System.out.println("sub:" +(a-b));
+        System.out.println("Multiple:" +(a*b));
+    }
+}

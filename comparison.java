86e3c160cdd3a1436ff763255859915b6c28f0f1
THANGAJEBINSONA <thangajebinson2006@gmail.com>
2025-03-13 11:47:42 +0530

    equal

---
+++ comparison.java
@@ -0,0 +1,14 @@
+public class comparison
+{
+    public static void main(String []args){
+        String str1="jebin";
+        String str2="jebin";
+        if(str1==str2){
+            System.out.println("it is equal");
+        }
+        else
+        {
+            System.out.println("it not equal");
+        }
+    }
+}

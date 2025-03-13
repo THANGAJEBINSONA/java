003569019a5952d8646b2812f56736cc2eb93461
THANGAJEBINSONA <thangajebinson2006@gmail.com>
2025-03-13 14:40:46 +0530

    age

---
+++ ifelse.java
@@ -0,0 +1,14 @@
+public class ifelse
+{
+    public static void main(String []args){
+        int age=16;
+        if(age>=18){
+        System.out.println("he is mager");
+
+        }
+        else{
+           System.out.println("he is not mager"); 
+        }
+        
+    }
+}

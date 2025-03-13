69f60a9bc461c748071c1ad359dbe8bb212a9297
THANGAJEBINSONA <thangajebinson2006@gmail.com>
2025-03-13 10:40:02 +0530

    double to int

---
+++ intprogram.java
@@ -0,0 +1,8 @@
+public class intprogram{
+    public static void main(String []args){
+        double mydouble=50.78d;
+        int value=(int)mydouble;
+        System.out.println(value);
+        
+    }
+}

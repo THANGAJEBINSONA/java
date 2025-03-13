b2f0b6bcf648a7e9df083f73da866148ab291a05
THANGAJEBINSONA <thangajebinson2006@gmail.com>
2025-03-13 12:04:57 +0530

    logi and

---
+++ logi1.java
@@ -0,0 +1,11 @@
+public class logi1
+{
+    public static void main(String []args)
+    {
+        int a=5,b=4;
+        if(a>b && a==b)
+        System.out.println("it is true");
+        else
+        System.out.println("false");
+    }
+}

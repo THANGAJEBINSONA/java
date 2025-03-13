65231f4be76e35a8b2a9a25ad71a2bb5df11932d
THANGAJEBINSONA <thangajebinson2006@gmail.com>
2025-03-13 14:01:26 +0530

    with temp

---
+++ swap2.java
@@ -0,0 +1,12 @@
+public class swap2
+{
+    public static void main(String []args)
+    {
+        int a=7,b=5;
+      int temp=a;
+        a=b;
+        b=temp;
+        
+        System.out.println("a" +a+ "b" +b );
+    }
+}

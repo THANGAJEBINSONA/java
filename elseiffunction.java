28156e59807d6611fff037242c4c89db6450a7f7
THANGAJEBINSONA <thangajebinson2006@gmail.com>
2025-03-13 15:16:52 +0530

    nestedif

---
+++ elseiffunction.java
@@ -0,0 +1,19 @@
+public class elseiffunction
+{
+    public static void main(String []args){
+        int age=16;
+        if(age==18){
+        System.out.println("he is mager");
+        }
+else if(age>=18)
+
+{
+     System.out.println("he is under 18 mager");
+    }
+        
+        else{
+           System.out.println("he is not mager"); 
+        }
+        
+    }
+}

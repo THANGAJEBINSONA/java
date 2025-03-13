0b1cb6ee1c93a3c2053a7ea1e0870a95da88575a
THANGAJEBINSONA <thangajebinson2006@gmail.com>
2025-03-13 10:04:46 +0530

    own program

---
+++ basic.java
@@ -0,0 +1,14 @@
+public class basic
+{
+    public static void main(String []args){
+        String name="gold";
+       System.out.println("Enter your name:" +name);
+       String sub="maths";
+       System.out.println("Enter the subject name:" +sub);
+       int mark=98;
+       System.out.println("Enter your mark:" +mark);
+       String grade="O";
+        System.out.print("Your Grade:" +grade);
+
+    }
+}

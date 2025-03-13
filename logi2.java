22e0778382c4659910af74fc789504429c6f174a
THANGAJEBINSONA <thangajebinson2006@gmail.com>
2025-03-13 12:07:05 +0530

    logi or

---
+++ logi2.java
@@ -0,0 +1,11 @@
+public class logi2
+{
+    public static void main(String []args)
+    {
+        int a=5,b=4;
+        if(a>b || a==b)
+        System.out.println("it is true");
+        else
+        System.out.println("false");
+    }
+}

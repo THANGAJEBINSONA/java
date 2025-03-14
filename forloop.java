80c156210df86d79cb156088493dcd23ef052977
THANGAJEBINSONA <thangajebinson2006@gmail.com>
2025-03-14 11:04:44 +0530

    increment

---
+++ forloop.java
@@ -0,0 +1,10 @@
+public class forloop{
+    public static void main(String []args)
+    {
+       int i;
+       for(i=0;i<5;i++)
+       {
+        System.out.println("Hi how are you");
+       }
+    }
+}

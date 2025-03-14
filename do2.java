02251929787c01c76f9431b459ff9614c7e1415e
THANGAJEBINSONA <thangajebinson2006@gmail.com>
2025-03-14 10:58:31 +0530

    string

---
+++ do2.java
@@ -0,0 +1,12 @@
+public class do2{
+    public static void main(String []args)
+    {
+        String name="\"#GOLD\"";
+        int i=0;
+        do{
+            System.out.println(name);
+            i++;
+        }
+        while(i<=5);
+    }
+}

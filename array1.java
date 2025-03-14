266a4c3ec39263524592b81cd684d9d8e7d4e372
THANGAJEBINSONA <thangajebinson2006@gmail.com>
2025-03-14 14:40:54 +0530

    rename

---
+++ array1.java
@@ -0,0 +1,9 @@
+public class array1{
+ public static void main(String []args){
+    String [] fruits={"Mango","Apple","Banana","Watermalan"};
+    System.out.println(fruits[2]="orange");
+    System.out.println(fruits[2]);
+ }
+     }
+
+ 

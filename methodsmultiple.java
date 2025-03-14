22c1620a92bf104fcb90ef8d8deb106d7fcc9dfd
THANGAJEBINSONA <thangajebinson2006@gmail.com>
2025-03-14 13:14:57 +0530

    methodmultiple

---
+++ methodsmultiple.java
@@ -0,0 +1,12 @@
+public class methodsmultiple{
+ static void mymethod()
+ {
+int a=7 ,b=3;
+System.out.println("multiple:" +a*b);
+
+ }
+ public static void main(String[]args)
+ {
+    mymethod();
+ }
+ }

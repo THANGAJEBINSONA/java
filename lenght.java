c02648c32aa3d7554f0a466e5c04ccd23a042cab
THANGAJEBINSONA <thangajebinson2006@gmail.com>
2025-03-14 13:56:10 +0530

    length

---
+++ lenght.java
@@ -0,0 +1,8 @@
+public class lenght{
+ public static void main(String []args){
+    String [] fruits={"Mango","Apple","Banana","Watermalan"};
+    System.out.println(fruits.length);
+ }
+     }
+
+ 

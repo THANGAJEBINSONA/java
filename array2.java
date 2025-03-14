d478800a718d896c6f0b78c3aa434051a6bf0280
THANGAJEBINSONA <thangajebinson2006@gmail.com>
2025-03-14 14:49:13 +0530

    fruits

---
+++ array2.java
@@ -0,0 +1,11 @@
+public class array2{
+ public static void main(String []args){
+    int n;
+    String [] fruits={"Mango","Apple","Banana","Watermalan"};
+    for(n=0;n<4;n++)
+    {    System.out.println(fruits[n]);
+ }
+     }
+}
+
+ 

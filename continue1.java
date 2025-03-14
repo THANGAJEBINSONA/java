3be919314153dce716d442b8e4c25d21f2421455
THANGAJEBINSONA <thangajebinson2006@gmail.com>
2025-03-14 11:48:49 +0530

    continue

---
+++ continue1.java
@@ -0,0 +1,14 @@
+public class continue1{
+    public static void main(String []args)
+    {
+       int i;
+       for(i=0;i<10;i++)
+       {
+        System.out.println(i);
+        if(i==7)
+        {
+            continue;
+        }
+       }
+    }
+}

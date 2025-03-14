2f28730886f6f0743d4d035d07f937a1d8b501d4
THANGAJEBINSONA <thangajebinson2006@gmail.com>
2025-03-14 10:15:03 +0530

    if

---
+++ nestedif.java
@@ -0,0 +1,17 @@
+public class nestedif{
+    public static void main(String []args){
+        String name="don";
+       
+        if(name.contains("don"))
+        {
+
+            System.out.println("it is a string");
+        }
+        else{
+            System.out.println("It is not a string");
+        }
+        
+        
+
+    }
+}

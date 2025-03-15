3f949599d4ca5473424f70c4b6bfbf68acb571a9
THANGAJEBINSONA <thangajebinson2006@gmail.com>
2025-03-15 11:05:42 +0530

    coffe

---
+++ coffee.java
@@ -0,0 +1,18 @@
+public class coffee{
+    public static void main(String[]args){
+        int coffee=4;
+        
+        switch (coffee){
+            case 1:System.out.println("black coffee");
+            break;
+            case 2:System.out.println("white coffee");
+            break;
+            case 3:System.out.println("choclate coffe");
+            break;
+            case 4:System.out.println("milk sake");
+            break;
+            default :
+            System.out.println("coffe is not avaliable");
+        } 
+    }
+}
